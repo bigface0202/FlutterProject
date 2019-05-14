7d8ca45b7c6feb2ff4fe598263e75cf23a47f0dc
Yusuke Kishishita <31816350+bigface0202@users.noreply.github.com>
2019-05-14 19:06:25 +0900

    Make the add button

--- udemy_study/lib/main.dart
+++ udemy_study/lib/main.dart
@@ -1,21 +1,6 @@
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 
 void main() {
-  var app = MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        //ここはヘッダー
-        title: Text('Lets see some images!'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        //ボタンが出来上がる
-        onPressed: () {
-          print('Hi there!');
-        },
-        child: Icon(Icons.clear), //childはボタンの中身
-      ),
-    ),
-  );
-
-  runApp(app);
+  runApp(App());
 }
--- udemy_study/lib/src/app.dart
+++ udemy_study/lib/src/app.dart
@@ -1,2 +1,34 @@
 import 'package:flutter/material.dart';
 
+class App extends StatefulWidget{
+  createState(){
+    return AppState();
+  }
+}
+
+//Statelessは画面が変わらない，データが変わらない
+// class App extends StatelessWidget{
+class AppState extends State<App>{
+  int counter = 0;
+
+  Widget build(context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Text('$counter'),
+        appBar: AppBar(
+        //ここはヘッダー
+        title: Text('Lets see some images!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        //ボタンが出来上がる
+        onPressed: () {
+          setState((){
+            counter += 1;
+          });
+        },
+        child: Icon(Icons.add), //childはボタンの中身
+      ),
+    ),
+  );
+  }
+}
