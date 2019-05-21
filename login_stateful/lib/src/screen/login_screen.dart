ec8dee85669f549ae2ce098195fb079dacf657c8
bigface0202 <theshoreunder@gmail.com>
2019-05-21 19:30:05 +0900

    Done of login state

--- login_stateful/lib/src/screen/login_screen.dart
+++ login_stateful/lib/src/screen/login_screen.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
+import '../mixins/validation_mixin.dart';
 
-class LoginScreen extends StatefulWidget{
-  createState(){
+class LoginScreen extends StatefulWidget {
+  createState() {
     return LoginScreenState();
   }
 }
 
-class LoginScreenState extends State<LoginScreen>{
-  Widget build(context){
+class LoginScreenState extends State<LoginScreen> with ValidationMixin {
+  final formKey = GlobalKey<FormState>();
+
+  String email = '';
+  String password = '';
+
+  Widget build(context) {
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Form(
+        key: formKey, //GrobalKeyを渡すことによって，入力される前と後で中身が変わる変数となる
         child: Column(
           children: [
             emailField(),
             passwordField(),
-            Container(margin:EdgeInsets.only(top:25.0)),
+            Container(margin: EdgeInsets.only(top: 25.0)),
             submitButton(),
           ],
         ),
@@ -23,29 +30,44 @@ class LoginScreenState extends State<LoginScreen>{
     );
   }
 
-  Widget emailField(){
+  Widget emailField() {
     return TextFormField(
       keyboardType: TextInputType.emailAddress,
       decoration: InputDecoration(
         labelText: 'Email address',
-        hintText:  'yourname@gmail.com',
+        hintText: 'yourname@gmail.com',
       ),
+      validator: validateEmail,
+      onSaved: (String value) {
+        email = value;
+      },
     );
   }
-  Widget passwordField(){
+
+  Widget passwordField() {
     return TextFormField(
       obscureText: true,
       decoration: InputDecoration(
         labelText: 'Enter password',
         hintText: 'Password',
       ),
+      validator: validatePassword(),
+      onSaved: (String value) {
+        password = value;
+      },
     );
   }
-  Widget submitButton(){
+
+  Widget submitButton() {
     return RaisedButton(
-    color: Colors.blueAccent,
-    child: Text('Submit'),
-    onPressed: (){},
+      color: Colors.blueAccent,
+      child: Text('Submit'),
+      onPressed: () {
+        if (formKey.currentState.validate()) {
+          formKey.currentState.save();
+          print('Time to post $email and $password to my API');
+        }
+      },
     );
   }
 }
