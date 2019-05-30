256b37ecd20e7bdaf5d79ffa972dcf61b7abe83f
Yusuke Kishishita <31816350+bigface0202@users.noreply.github.com>
2019-05-30 19:26:23 +0900

    Done the section

--- login_bloc/lib/src/blocs/bloc.dart
+++ login_bloc/lib/src/blocs/bloc.dart
@@ -1,7 +1,17 @@
 import 'dart:async';
+import 'validators.dart';
 
-class Bloc{
-  final emailController = StreamController<String>();
-  final passwordController = StreamController<String>();
+class Bloc extends Object with  Validators {
+  //アンダースコアつけるとprivateになる
+  //classで引っ張った時に見えなくなる
+  final _email = StreamController<String>();
+  final _password = StreamController<String>();
 
+  // Add data to stream
+  Stream<String> get email => _email.stream.transform(validateEmail);
+  Stream<String> get password => _password.stream.transform(validatePassword);
+
+  // Change data
+  Function(String) get changeEmail => _email.sink.add;
+  Function(String) get changePassword => _password.sink.add;
 }
