66a3b33dca97940135ece66729791d47a60f8e97
Yusuke Kishishita <31816350+bigface0202@users.noreply.github.com>
2019-06-15 20:04:59 +0900

    progress

--- news/lib/src/blocs/stories_bloc.dart
+++ news/lib/src/blocs/stories_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:rxdart/rxdart.dart';
 import '../models/item_model.dart';
 import '../resources/repository.dart';
+import 'dart:async';
 
 class StoriesBloc{
   final _repository = Repository();
@@ -12,6 +13,17 @@ class StoriesBloc{
   fetchTopIds() async {
     final ids = await _repository.fetchTopIds();
     _topIds.sink.add(ids);
+  }
+  
+  //cache mapにIDで識別されるニュースを振り分ける
+  _itemsTransformer(){
+    return ScanStreamTransformer(
+      (Map<int, Future<ItemModel>> cache, int id, _) {
+        cache[id] = _repository.fetchItem(id);
+        return cache;
+      },
+      <int, Future<ItemModel>>{},
+    );
   }
 
   dispose(){
