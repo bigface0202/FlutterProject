eb595ed24900af27ad6a1554807d083140575e9a
yusuke <theshoreunder@gmail.com>
2020-06-26 20:19:23 +0900

    update

--- bluetooth_iot/lib/bluetooth.dart
+++ bluetooth_iot/lib/bluetooth.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'dart:convert';
 
 class BluetoothApp extends StatefulWidget {
   @override
@@ -97,20 +98,6 @@ class _BluetoothAppState extends State<BluetoothApp> {
     });
   }
 
-  void _disconnect() async {
-    // Bluetoothの接続を閉じる
-    await connection.close();
-    Scaffold.of(context).showSnackBar(
-      new SnackBar(content: new Text('Device disconnected')),
-    );
-    // [_connected]の変数を更新
-    if (!connection.isConnected) {
-      setState(() {
-        _connected = false;
-      });
-    }
-  }
-
   List<DropdownMenuItem<BluetoothDevice>> _getDeviceItems() {
     List<DropdownMenuItem<BluetoothDevice>> items = [];
     if (_devicesList.isEmpty) {
@@ -132,40 +119,171 @@ class _BluetoothAppState extends State<BluetoothApp> {
     return items;
   }
 
+  // Bluetoothデバイスをオンにするメッセージを送る
+  // "1"を送ることでオンにする
+  void _sendOnMessageToBluetooth() async {
+    connection.output.add(utf8.encode("1" + "\r\n"));
+    await connection.output.allSent;
+    Scaffold.of(context).showSnackBar(
+      new SnackBar(content: new Text('Device Turned On')),
+    );
+    setState(() {
+      _deviceState = 1;
+    });
+  }
+
+  // Bluetoothデバイスをオフにするメッセージを送る
+  // "0"を送ることでオフにする
+  void _sendOffMessageToBluetooth() async {
+    connection.output.add(utf8.encode("0" + "\r\n"));
+    await connection.output.allSent;
+    Scaffold.of(context).showSnackBar(
+      new SnackBar(content: new Text('Device Turned Off')),
+    );
+    setState(() {
+      _deviceState = -1;
+    });
+  }
+
+  void _connect() async {
+    if (_device == null) {
+      Scaffold.of(context).showSnackBar(
+        new SnackBar(content: new Text('No device selected')),
+      );
+    } else {
+      if (!isConnected) {
+        // そのアドレスを使ってデバイスに繋げる事を試みる
+        await BluetoothConnection.toAddress(_device.address)
+            .then((_connection) {
+          print('Connected to device');
+          connection = _connection;
+          // デバイスの接続状況を更新
+          setState(() {
+            _connected = true;
+          });
+          connection.input.listen(null).onDone(() {
+            if (isDisconnecting) {
+              print('Disconnecting locally!');
+            } else {
+              print('Disconnected remotely!');
+            }
+            if (this.mounted) {
+              setState(() {});
+            }
+          });
+        }).catchError((error) {
+          print('Cannot connect, exception occurred');
+          print(error);
+        });
+        Scaffold.of(context).showSnackBar(
+          new SnackBar(content: new Text('Device connected')),
+        );
+      }
+    }
+  }
+
+  void _disconnect() async {
+    // Bluetoothの接続を閉じる
+    await connection.close();
+    Scaffold.of(context).showSnackBar(
+      new SnackBar(content: new Text('Device disconnected')),
+    );
+    // [_connected]の変数を更新
+    if (!connection.isConnected) {
+      setState(() {
+        _connected = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Switch(
-          value: _bluetoothState.isEnabled,
-          onChanged: (bool value) {
-            future() async {
-              if (value) {
-                // Bluetooth起動
-                await FlutterBluetoothSerial.instance.requestEnable();
-              } else {
-                // Bluetooth切る
-                await FlutterBluetoothSerial.instance.requestDisable();
-              }
-              // デバイスリストをリロードする
-              await getPairedDevices();
-              _isButtonUnavailable = false;
-              // Bluetoothを切る前に全てのデバイスと接続を切る
-              if (_connected) {
-                _disconnect();
-              }
-            }
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Text(
+                "Enable Bluetooth",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Switch(
+                value: _bluetoothState.isEnabled,
+                onChanged: (bool value) {
+                  future() async {
+                    if (value) {
+                      // Bluetooth起動
+                      await FlutterBluetoothSerial.instance.requestEnable();
+                    } else {
+                      // Bluetooth切る
+                      await FlutterBluetoothSerial.instance.requestDisable();
+                    }
+                    // デバイスリストをリロードする
+                    await getPairedDevices();
+                    _isButtonUnavailable = false;
+                    // Bluetoothを切る前に全てのデバイスと接続を切る
+                    if (_connected) {
+                      _disconnect();
+                    }
+                  }
 
-            future().then((_) {
-              setState(() {});
-            });
-          },
+                  future().then((_) {
+                    setState(() {});
+                  });
+                },
+              ),
+            ],
+          ),
         ),
-        DropdownButton(
-          items: _getDeviceItems(),
-          onChanged: (value) => setState(() => _device = value),
-          value: _devicesList.isNotEmpty ? _device : null,
+        Center(
+          child: Text(
+            'PAIRED DEVICES',
+            style: TextStyle(fontSize: 24),
+          ),
         ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Text('Device:'),
+              DropdownButton(
+                items: _getDeviceItems(),
+                onChanged: (value) => setState(() => _device = value),
+                value: _devicesList.isNotEmpty ? _device : null,
+              ),
+              RaisedButton(
+                onPressed: _isButtonUnavailable
+                    ? null
+                    : _connected ? _disconnect : _connect,
+                child: Text(_connected ? 'Disconnect' : 'Connect'),
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Card(
+            child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Text(
+                  'Device 1',
+                  style: TextStyle(fontSize: 18),
+                ),
+                FlatButton(
+                  onPressed: _connected ? _sendOnMessageToBluetooth : null,
+                  child: Text('ON'),
+                ),
+                FlatButton(
+                  onPressed: _connected ? _sendOffMessageToBluetooth : null,
+                  child: Text('OFF'),
+                )
+              ],
+            ),
+          ),
+        )
       ],
     );
   }
